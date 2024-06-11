2a607464e631798162565b98f85e5263cf565af8
Harrison,Hao <tsworld1314@gmail.com>
2024-06-11 14:04:53 +0000

    [IO_URING] Add a new example.

--- io_uring_polling.c
+++ io_uring_polling.c
@@ -1,85 +1,88 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <poll.h>
-#include <unistd.h>
 #include <liburing.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
-#define BUFFER_SIZE 4096
+#define BUFFER_SIZE 1024
 
-int main() {
+size_t my_fread(void *restrict buffer, size_t size, size_t count, FILE *restrict stream) {
     struct io_uring ring;
-    struct io_uring_sqe *sqe;
-    struct io_uring_cqe *cqe;
-    char buffer[BUFFER_SIZE];
-    int ret;
+    struct io_uring_params params;
+    memset(&params, 0, sizeof(params));
+    params.flags = IORING_SETUP_SQPOLL;
+    params.sq_thread_idle = 2000;  // SQPOLL线程空闲时间，以毫秒为单位
 
-    // Initialize io_uring
-    ret = io_uring_queue_init(8, &ring, 0);
-    if (ret) {
-        fprintf(stderr, "io_uring setup failed: %d\n", ret);
+    if (io_uring_queue_init_params(32, &ring, &params) < 0) {
+        perror("io_uring_queue_init_params");
+        return 0;
+    }
+
+    int fd = fileno(stream);
+    if (fd < 0) {
+        perror("fileno");
+        return 0;
+    }
+
+    struct io_uring_sqe *sqe = io_uring_get_sqe(&ring);
+    if (!sqe) {
+        fprintf(stderr, "Unable to get sqe\n");
+        io_uring_queue_exit(&ring);
+        return 0;
+    }
+
+    struct iovec iov = {
+        .iov_base = buffer,
+        .iov_len = size * count
+    };
+
+    io_uring_prep_readv(sqe, fd, &iov, 1, 0);
+
+    io_uring_submit(&ring);
+
+    struct io_uring_cqe *cqe;
+    int ret = io_uring_wait_cqe(&ring, &cqe);
+    if (ret < 0) {
+        fprintf(stderr, "Error waiting for completion: %s\n", strerror(-ret));
+        io_uring_queue_exit(&ring);
+        return 0;
+    }
+
+    if (cqe->res < 0) {
+        fprintf(stderr, "Async readv failed.\n");
+        io_uring_cqe_seen(&ring, cqe);
+        io_uring_queue_exit(&ring);
+        return 0;
+    }
+
+    size_t bytes_read = cqe->res;
+    io_uring_cqe_seen(&ring, cqe);
+    io_uring_queue_exit(&ring);
+
+    return bytes_read / size;
+}
+
+int main(int argc, char *argv[]) {
+    FILE *fp = fopen("CMakeLists.txt", "r");
+    if (!fp) {
+        perror("fopen");
         return 1;
     }
 
-    do {
-        // Get a submission queue entry for polling
-        sqe = io_uring_get_sqe(&ring);
-        if (!sqe) {
-            fprintf(stderr, "Could not get SQE.\n");
+    char buffer[BUFFER_SIZE];
+    size_t n = 0;
+
+    int times = 0;
+    while ((n = my_fread(buffer, sizeof(char), BUFFER_SIZE, fp))) {
+        buffer[n] = '\0'; // Null-terminate the buffer to safely print it
+        printf("%s", buffer);
+        
+        times++;
+        if(times > 2)
             break;
-        }
+    }
 
-        // Prepare to poll stdin (fd 0) until it is ready to be read
-        io_uring_prep_poll_add(sqe, 0, POLLIN);
-
-        // Submit the poll request
-        io_uring_submit(&ring);
-
-        // Wait for poll completion
-        ret = io_uring_wait_cqe(&ring, &cqe);
-        if (ret) {
-            fprintf(stderr, "Error waiting for poll completion: %d\n", ret);
-            break;
-        }
-
-        if (cqe->res < 0) {
-            fprintf(stderr, "Polling failed: %d\n", cqe->res);
-            io_uring_cqe_seen(&ring, cqe);
-            break;
-        }
-
-        // Mark this poll CQE as seen
-        io_uring_cqe_seen(&ring, cqe);
-
-        // Prepare for reading after successful polling
-        sqe = io_uring_get_sqe(&ring);
-        io_uring_prep_read(sqe, 0, buffer, BUFFER_SIZE, 0); // Read from stdin
-
-        // Submit the read request
-        io_uring_submit(&ring);
-
-        // Wait for read completion
-        ret = io_uring_wait_cqe(&ring, &cqe);
-        if (ret) {
-            fprintf(stderr, "Error waiting for read completion: %d\n", ret);
-            break;
-        }
-
-        if (cqe->res < 0) {
-            fprintf(stderr, "Read failed: %d\n", cqe->res);
-            break;
-        }
-
-        // Output the read data to stdout
-        write(STDOUT_FILENO, buffer, cqe->res);
-
-        // Mark this read CQE as seen
-        io_uring_cqe_seen(&ring, cqe);
-
-    } while (cqe->res > 0); // Continue if data was read
-
-    // Cleanup
-    io_uring_queue_exit(&ring);
-
+    fclose(fp);
     return 0;
 }
