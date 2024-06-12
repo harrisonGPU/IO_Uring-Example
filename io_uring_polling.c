d594e01b42dc2991aff86f1164ab071a725d4c5d
Harrison,Hao <tsworld1314@gmail.com>
2024-06-12 13:28:28 +0000

    [IO_URING] Update fclose and fopen function.

--- io_uring_polling.c
+++ io_uring_polling.c
@@ -6,9 +6,16 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE 1024
-#define QUEUE_DEPTH 64
+#define BUFFER_SIZE 1024  // Define the size of the buffer for read operations
+#define QUEUE_DEPTH 64    // Define the depth of the io_uring queue
 
+// Structure to encapsulate a FILE stream and an io_uring instance
+typedef struct {
+    FILE *stream;
+    struct io_uring ring;
+} my_file;
+
+// Function to print the status of the io_uring kernel thread
 void print_sq_poll_kernel_thread_status() {
     if (system("ps --ppid 2 | grep io_uring-sq") == 0)
         printf("Kernel thread io_uring-sq found running...\n");
@@ -16,8 +23,84 @@ void print_sq_poll_kernel_thread_status() {
         printf("Kernel thread io_uring-sq is not running.\n");
 }
 
-size_t my_fread(void *restrict buffer, size_t size, size_t count, FILE *restrict stream, struct io_uring *ring) {
-    int fd = fileno(stream);
+// Fopen function to open a file with io_uring setup
+my_file *my_fopen(const char *filename, const char *mode) {
+    struct io_uring ring;
+    struct io_uring_params params;
+    memset(&params, 0, sizeof(params));
+
+    // Set up io_uring to use SQ polling
+    params.flags = IORING_SETUP_SQPOLL;
+    params.sq_thread_idle = 2000;
+
+    // Initialize the io_uring queue with the specified parameters
+    if (io_uring_queue_init_params(QUEUE_DEPTH, &ring, &params) < 0) {
+        perror("io_uring_queue_init_params");
+        return NULL;
+    }
+
+    // Check for root privileges required for setting up SQPOLL
+    if (geteuid()) {
+        fprintf(stderr, "You need root privileges to run this program.\n");
+        io_uring_queue_exit(&ring);
+        return NULL;
+    }
+
+    FILE *fp = fopen(filename, mode);
+    if (!fp) {
+        perror("fopen");
+        io_uring_queue_exit(&ring);
+        return NULL;
+    }
+
+    int fd = fileno(fp);
+    if (fd < 0) {
+        perror("fileno");
+        fclose(fp);
+        io_uring_queue_exit(&ring);
+        return NULL;
+    }
+
+    // Retrieve and set file descriptor flags for non-blocking mode
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (flags == -1) {
+        perror("fcntl(F_GETFL)");
+        fclose(fp);
+        io_uring_queue_exit(&ring);
+        return NULL;
+    }
+
+    if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1) {
+        perror("fcntl(F_SETFL)");
+        fclose(fp);
+        io_uring_queue_exit(&ring);
+        return NULL;
+    }
+
+    // Register the file descriptor with io_uring
+    if (io_uring_register_files(&ring, &fd, 1) < 0) {
+        perror("io_uring_register_files");
+        fclose(fp);
+        io_uring_queue_exit(&ring);
+        return NULL;
+    }
+
+    // Allocate memory for the custom file structure
+    my_file *mf = malloc(sizeof(my_file));
+    if (mf) {
+        mf->stream = fp;
+        mf->ring = ring;
+    } else {
+        fclose(fp);
+        io_uring_queue_exit(&ring);
+    }
+
+    return mf;
+}
+
+// Fread function to perform asynchronous read using io_uring
+size_t my_fread(void *restrict buffer, size_t size, size_t count, my_file *restrict mf) {
+    int fd = fileno(mf->stream);
     if (fd < 0) {
         perror("fileno");
         return 0;
@@ -28,18 +111,17 @@ size_t my_fread(void *restrict buffer, size_t size, size_t count, FILE *restrict
         .iov_len = size * count
     };
 
-    struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
+    struct io_uring_sqe *sqe = io_uring_get_sqe(&mf->ring);
     if (!sqe) {
         fprintf(stderr, "Unable to get sqe\n");
         return 0;
     }
 
     io_uring_prep_readv(sqe, fd, &iov, 1, 0);
-
-    io_uring_submit(ring);
+    io_uring_submit(&mf->ring);
 
     struct io_uring_cqe *cqe;
-    int ret = io_uring_wait_cqe(ring, &cqe);
+    int ret = io_uring_wait_cqe(&mf->ring, &cqe);
     if (ret < 0) {
         fprintf(stderr, "Error waiting for completion: %s\n", strerror(-ret));
         return 0;
@@ -47,79 +129,37 @@ size_t my_fread(void *restrict buffer, size_t size, size_t count, FILE *restrict
 
     if (cqe->res < 0) {
         fprintf(stderr, "Async readv failed: %s\n", strerror(-cqe->res));
-        io_uring_cqe_seen(ring, cqe);
+        io_uring_cqe_seen(&mf->ring, cqe);
         return 0;
     }
 
     size_t bytes_read = cqe->res;
-    io_uring_cqe_seen(ring, cqe);
-
+    io_uring_cqe_seen(&mf->ring, cqe);
     return bytes_read / size;
 }
 
+// Close the file structure and clean up io_uring resources
+void my_close(my_file *mf) {
+    if (mf) {
+        if (mf->stream) {
+            fclose(mf->stream);
+        }
+        io_uring_queue_exit(&mf->ring);
+        free(mf);
+    }
+}
+
 int main(int argc, char *argv[]) {
-    struct io_uring ring;
-    struct io_uring_params params;
-    memset(&params, 0, sizeof(params));
-    
-    params.flags = IORING_SETUP_SQPOLL;
-    params.sq_thread_idle = 2000;
-
-    if (io_uring_queue_init_params(QUEUE_DEPTH, &ring, &params) < 0) {
-        perror("io_uring_queue_init_params");
-        return 1;
-    }
-
-    if (geteuid()) {
-        fprintf(stderr, "You need root privileges to run this program.\n");
-        io_uring_queue_exit(&ring);
-        return 1;
-    }
-
-    print_sq_poll_kernel_thread_status();
-
-    FILE *fp = fopen("CMakeLists.txt", "r");
-    if (!fp) {
-        perror("fopen");
-        io_uring_queue_exit(&ring);
-        return 1;
-    }
-
-    int fd = fileno(fp);
-    if (fd < 0) {
-        perror("fileno");
-        fclose(fp);
-        io_uring_queue_exit(&ring);
-        return 1;
-    }
-
-    int flags = fcntl(fd, F_GETFL, 0);
-    if (flags == -1) {
-        perror("fcntl(F_GETFL)");
-        fclose(fp);
-        io_uring_queue_exit(&ring);
-        return 1;
-    }
-
-    if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1) {
-        perror("fcntl(F_SETFL)");
-        fclose(fp);
-        io_uring_queue_exit(&ring);
-        return 1;
-    }
-
-    if (io_uring_register_files(&ring, &fd, 1) < 0) {
-        perror("io_uring_register_files");
-        fclose(fp);
-        io_uring_queue_exit(&ring);
+    my_file *mf = my_fopen("CMakeLists.txt", "r");
+    if (!mf) {
         return 1;
     }
 
     char buffer[BUFFER_SIZE];
     size_t n = 0;
-
     int times = 0;
-    while ((n = my_fread(buffer, sizeof(char), BUFFER_SIZE, fp, &ring)) > 0) {
+
+    while ((n = my_fread(buffer, sizeof(char), BUFFER_SIZE, mf)) > 0) {
         buffer[n] = '\0'; // Null-terminate the buffer to safely print it
         printf("%s", buffer);
 
@@ -128,9 +168,8 @@ int main(int argc, char *argv[]) {
             break;
     }
 
-    fclose(fp);
+    my_close(mf);
     print_sq_poll_kernel_thread_status();
-    io_uring_queue_exit(&ring);
 
     return 0;
 }
